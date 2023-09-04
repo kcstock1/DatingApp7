bf002f97ea233b6a0099d1a95d5c218a94ec4aff
Kevin Stock <kstock@hylaine.com>
2023-09-04 15:17:53 -0400

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnections"));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
